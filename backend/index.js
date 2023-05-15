be38b413e95b2a4217afd28c90da7bc6e9729564
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-05-15 12:05:25 +0530

    delete : circular connection of config folder creating issue in deployment

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 require('dotenv').config()
 const cors = require("cors");
 const app = express();
-const {connection} = require("./Config/db.js");
 const { EventRouter } = require("./Routes/Event.route.js");
 const {UserRouter} = require("./Routes/User.route.js");
 const { authentication } = require("./Middlewears/authMiddlewear.js");
 const { checkExpired } = require("./Middlewears/checkEvent.js");
+mongoose.set('strictQuery', false);
 
 app.use(express.json());
 app.use(cors({ origin: true }));
@@ -19,8 +19,9 @@ app.use("/event",EventRouter)
 
 app.listen(process.env.PORT || 5000,async()=>{
     try {
-        await connection;
-        console.log(`server has been connected to ${process.env.PORT}`)
+
+await mongoose.connect(process.env.MONGO_DB);
+  console.log(`server has been connected to ${process.env.PORT || 5000}`)
     } catch (error) {
         console.log(error);
     }
