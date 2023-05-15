198e8b0fbd48de71758e02d8068beb67fefc4dc9
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-05-15 14:25:51 +0530

    delete : config folder

--- backend/Config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-require('dotenv').config()
-mongoose.set('strictQuery', false);
-
-const connection = mongoose.connect(process.env.MONGO_DB);
-
-module.exports = {connection}
