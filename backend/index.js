5ea3146f486e1e9ae664022c1a6104952aa4ffd8
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-05-15 12:09:36 +0530

    fix : added mongoose in the index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv').config()
 const cors = require("cors");
+const mongoose = require("mongoose")
 const app = express();
 const { EventRouter } = require("./Routes/Event.route.js");
 const {UserRouter} = require("./Routes/User.route.js");
